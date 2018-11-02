a7658f2421cbf46504b9cccb0df6eac8896172e2
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-02 20:50:32 +0330

    migration script, once in a setup

---
+++ app/migrate.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const questions = require('./storage/questions.json');
+const Question = require('./models/Question.js');
+
+// mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/questioner', {
+    useNewUrlParser: true
+});
+mongoose.connection
+    .once('open', init)
+    .on('error', console.error);
+
+function init() {
+    function inject(n) {
+        let question = new Question(questions[n]);
+        console.log(questions[n]);
+        console.log(n);
+        question.save().then(() => {
+            n++;
+            if (n == questions.length) {
+                console.log(n+" questions added to database");
+                mongoose.connection.close();
+            } else {
+                inject(n);
+            }
+        });
+    }
+    inject(0);
+
+}
