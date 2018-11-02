e5c8a4a8f6a01f63f782f8ed6272321c490c138d
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-02 19:22:19 +0330

    auth middleware  completed (checking token,user,question)

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,19 +1,46 @@
 const User = require('../models/User.js');
+const Question = require('../models/Question.js');
 module.exports = function (req, res, next) {
     if (req.body.token) {
         User.findOne({
             token: req.body.token
         }).then((user) => {
             if (user) {
-                res.locals.user = user;
-                next();
-            } else {
-                res.redirect('/');
-            }
+                Question.findOne({
+                    id: question.params.qid
+                }).then((question) => {
+                    if (question) {
+                        if (question.prev) {
+                            if (user.sheet.hasOwnProperty(question.prev)) {
+                                next();
+                            } else {
+                                res.json({
+                                    err: 'your not allowed to answer this question at the time',
+                                    next: '/' + question.prev
+                                });
+                            }
+                        } else {
+                            next();
+                        }
+                    } else {
+                        res.json({
+                            err: 'question not found',
+                            next: '/'
+                        });
+                    }
+                });
 
+            } else {
+                res.json({
+                    err: 'token not found!, you should first visit "/" to get a new token',
+                    next: '/'
+                });
+            }
         });
-        next();
     } else {
-        res.redirect('/');
+        res.json({
+            err: 'you should first visit "/" to login, othewise the case is that you didn`t pass the token',
+            next: '/'
+        });
     }
 }
