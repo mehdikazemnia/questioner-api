a6f9279c3b087a5a4c086290fb8cb2f8331acd90
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-02 20:17:05 +0330

    token generation and join process added

--- app/QuestionManager.js
+++ app/QuestionManager.js
@@ -1,4 +1,5 @@
 const Question = require('./models/Question.js');
+const User = require('./models/User.js');
 class QuestionManager {
 
     /**
@@ -9,12 +10,52 @@ class QuestionManager {
     }
 
     /**
+     * generates a random token
+     * again and again if already exists
+     */
+    generateToken(clb) {
+        let rand = Date.now() + Math.round((Math.random() * 1000)) + 1000;
+        token = 't' + rand.toString(32);
+        User.findOne({
+            token: token
+        }).then((user) => {
+            if (user) {
+                this.generateToken(clb);
+            } else {
+                clb(token);
+            }
+        });
+
+    }
+
+    /**
+     * join the user, send the generated token
+     * @param {Object} req - express
+     * @param {Object} res - express
+     */
+    join(req, res) {
+        this.generateToken((token) => {
+            user = new User({
+                token: token,
+                sheet: {}
+            });
+            user.save().then(() => {
+                res.json({
+                    msg: 'welcome, save your token and send it with all POST requests',
+                    token: token,
+                    next: '/' + this.fqid
+                });
+            });
+        });
+    }
+
+    /**
      * render get requests
      * @param {Object} req - express
      * @param {Object} res - express
      */
     render(req, res) {
-        
+
     }
 
     /**
